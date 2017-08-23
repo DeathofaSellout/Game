e666bca2925541b3da03421771172facf30e0da8
deathofasellout <javascriptforlife@gmail.com>
2017-08-23 14:37:19 -0700

    Working game

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 $(document).ready(function(){
-  $(window).load(function() {
+  let marioSteps = 80; //starting point for mario
+  let princessSteps = 80; //starting point for princess
+  let width = $(window).width() - 20;
 
-  	var theWindow        = $(window),
-  	    $bg              = $("#bg"),
-  	    aspectRatio      = $bg.width() / $bg.height();
+  $(document).on("keyup", function(event){
 
-  	function resizeBg() {
+    if(marioSteps >= (width - 100)){
+      alert("Mario has won!!!!")
+    } else if( princessSteps >= (width - 100)){
+      alert("The Princess has won!!!");
+    }
+    if(event.which === 16){
+      princessSteps = princessSteps + 20;
 
-  		if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
-  		    $bg
-  		    	.removeClass()
-  		    	.addClass('bgheight');
-  		} else {
-  		    $bg
-  		    	.removeClass()
-  		    	.addClass('bgwidth');
-  		}
+      //left shift keypress princess
+      if(princessSteps < (width - 100)){
+        $("#princess").animate({
+          "margin-left": "+=20px"
+        }, 5);
+      }
+    } else if(event.which === 39 ){
+      marioSteps += 20;
 
-  	}
-
-  	theWindow.resize(resizeBg).trigger("resize");
-
+      //left right arrow keypress mario
+      if(princessSteps < (width - 100)){
+        $("#mario").animate({
+          "margin-left": "+=20px"
+        }, 5);
+      }
+    }
+   // do animate step instead jQuery
+    //event.which shows keycode
   });
-
 });
---
+++ readme.md
@@ -0,0 +1,19 @@
+Quick description of what this repo is about.
+
+See the published project at github.com/sf-wdi-labs/readme-example!
+
+Technologies Used
+
+jQuery
+HTML
+CSS
+Code I'm Proud Of
+
+  function allTheCatz(inputCatz) {
+    Catz.create(inputCatz);
+  };
+I'm proud of this code because it required me to trace complex data from the client-side all the way back to the database. I also found and used the .create method. I hadn't seen it before, but once I figured it out it worked better for me than ____ because ____ ....
+
+Screen Shots
+
+Take screen shots using control command shift 4. The image will be on your clipboard and you can paste it where you like with command v. If you paste it anywhere within a github text editing window, it will give you a markdown version.
